f5af9ed9e095e0f6c2af09cc079d22db75d8b9d4
AkyunaAkish <akyunaakish@gmail.com>
2015-07-22 15:43:12 -0600

    added serve-favicon module

--- index.js
+++ index.js
@@ -13,6 +13,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var favicon = require('serve-favicon');
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
