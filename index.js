00e7af86fc54ae18b135c6effcf6c9c25e996dbc
AkyunaAkish <akyunaakish@gmail.com>
2015-07-22 16:22:36 -0600

    trying to fix it for heroku

--- index.js
+++ index.js
@@ -10,10 +10,14 @@
 //    console.log('listening on *:3000');
 //});
 
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var favicon = require('serve-favicon');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
@@ -25,6 +29,9 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+var port = process.env.PORT || 3000;
+
+
+http.listen(port, function(){
+    console.log('listening');
 });
